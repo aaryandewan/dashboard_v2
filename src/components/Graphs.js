d3da27d1a1134bfb8d5b861df08d72028fbba5c0
Aaryan Dewan <aaryandewan@yahoo.com>
2021-07-03 20:01:07 +0530

    complete graphs

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -9,7 +9,54 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  BarChart,
+  Bar,
 } from "recharts";
+
+const dataForBarChart = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 const data = [
   {
@@ -56,9 +103,9 @@ const data = [
   },
 ];
 
-export default function Graphs() {
+export function GraphLine() {
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="100%" height={300}>
       <LineChart
         width={800}
         height={500}
@@ -83,6 +130,32 @@ export default function Graphs() {
         />
         {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
       </LineChart>
+    </ResponsiveContainer>
+  );
+}
+
+export function GraphBar() {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart
+        width={600}
+        height={500}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        {/* <CartesianGrid strokeDasharray="3 3" /> */}
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="pv" fill="#8884d8" />
+        <Bar dataKey="uv" fill="#82ca9d" />
+      </BarChart>
     </ResponsiveContainer>
   );
 }
